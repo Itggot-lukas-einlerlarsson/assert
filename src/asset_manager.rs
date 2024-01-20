c69c31dd6d416d82dbc4ff9d5c5acac18a6f5f81
Lukas Einler Larsson <lukas.einlerlarsson@itggot.se>
2024-01-20 17:45:59 +0100

    updated struct

--- src/asset_manager.rs
+++ src/asset_manager.rs
@@ -1,13 +1,16 @@
 pub struct AssetManager {
-    programname : String, 
+    programname : String,
     assets : Vec<String>
 }
 
 impl AssetManager {
-    pub func run(pn : &String) {
+    pub fn new(pn : &String) -> AssetManager {
         return AssetManager {
             programname : String::from(pn),
             assets : Vec::new()
-        }   
+        }
+    }
+    pub fn print_pn(&self) {
+        println!("Hej from {}", self.programname);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod asset_manager;
+use asset_manager::AssetManager;
+
 fn main() {
+    let s = String::from("assert");
+    let test = AssetManager::new(&s);
+    test.print_pn();
     println!("Hello, world!");
 }
