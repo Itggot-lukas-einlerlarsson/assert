64f5cfe74c1bed128eae84b2a14126991a70dae7
Lukas Einler Larsson <lukas.einerlarsson@saabgroup.com>
2024-03-29 12:41:33 +0100

    added reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod asset_manager;
 use asset_manager::AssetManager;
 
-fn main() {
+// tokio let's us use "async" on our main function
+#[tokio::main]
+async fn main() {
     let s = String::from("assert");
     let test = AssetManager::new(&s);
+    let result = reqwest::get("https://api.spotify.com/v1/search").await;
     test.print_pn();
-    println!("Hello, world!");
+    println!("{:?}", result);
 }
+
